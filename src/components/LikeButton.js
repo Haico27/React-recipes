f6618bc1a55dd8cb13753d6981b0040971ac6770
Haico27 <haico.wensink@gmail.com>
2017-05-03 14:40:23 +0700

    created LikeButton, does not work properly yet

---
+++ src/components/LikeButton.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react'
+import './LikeButton.sass'
+
+class LikeButton extends PureComponent {
+
+  constructor() {
+    super()
+
+    this.state = {
+      liked: false
+    }
+  }
+
+// function that toggles the className of the component
+  classNames() {
+    const { liked } = this.state
+    let classes = 'like'
+
+    if (liked) { classes += 'liked' }
+
+    return classes
+  }
+
+  // function that toggles the LikeButton
+  toggleLike() {
+    console.log("Like button clicked!!!")
+  }
+
+  render() {
+    const liked = false
+    return(
+      <p className={ this.classNames() }>
+        <button onClick={ this.toggleLike.bind(this) }>
+          { liked ? '❤️' : '♡' }
+        </button>
+        <span className="likes">{ liked ? 'You like this' : null }</span>
+      </p>
+    )
+  }
+}
+
+export default LikeButton
