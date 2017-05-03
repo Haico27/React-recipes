6097e5afe114e4a994d4f44785dc98d3d471e338
Haico27 <haico.wensink@gmail.com>
2017-05-03 21:47:31 +0700

    lifting state up, not working yet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.sass'
 
 const recipes = [
   {
-    _id: 123,
+    _id: '123',
     title: 'Creamy Vegan Linguine with Wild Mushrooms',
     summary: 'How do we make these noodles so creamy and cheesy without cream or cheese? Click to learn our secrets.',
     vegan: true,
@@ -14,7 +14,7 @@ const recipes = [
     liked: false,
   },
   {
-    _id: 124,
+    _id: '124',
     title: 'Ratatouille',
     summary: 'Delicious vegan Ratatouille with a lot of vegetables and a true pleasure for the tongue. Paradise is just 1 hour away.',
     vegan: true,
@@ -24,7 +24,7 @@ const recipes = [
     liked: false,
   },
   {
-    _id: 125,
+    _id: '125',
     title: 'Three Bean Chili with Spring Pesto',
     summary: 'Three types of beans—chickpeas, cannellini, and kidney—ensures this chili is filling and full of protein.',
     vegan: true,
@@ -34,7 +34,7 @@ const recipes = [
     liked: false,
   },
   {
-    _id: 126,
+    _id: '126',
     title: 'Rainbow Vegetarian Pad Thai with Peanuts and Basil',
     summary: 'It is brown rice noodles (because we’re currently doing the sugar free experiment again and loosely avoiding refined grains) and spiralized veggies (because veggies in noodle form feels like more noodles) and a super tangy-delicious Pad Thai sauce that you just shake up in a jar in about five seconds flat, and peanuts that almost instantly start to soak up the sauce, and a gently scrambled egg that kind of cream-ifies the whole thing.',
     vegan: false,
@@ -44,7 +44,7 @@ const recipes = [
     liked: false,
   },
   {
-    _id: 127,
+    _id: '127',
     title: 'Grilled Salmon',
     summary: 'A simple soy sauce and brown sugar marinade, with hints of lemon and garlic, are the perfect salty-sweet complement to rich salmon fillets. Even my 9 year old loves this recipe!',
     vegan: false,
@@ -62,6 +62,10 @@ class App extends React.Component {
     this.state = {
       recipes: recipes
     }
+  }
+
+  updateRecipe(id, update) {
+    
   }
 
 
