5c61637ef6db934b23b74cbaf46aa7a2862e48c3
Haico27 <haico.wensink@gmail.com>
2017-05-19 01:01:45 +0700

    created signup component, route and action

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Router, Route, IndexRoute } from 'react-router'
 import App from './App'
 import RecipesContainer from './recipes/RecipesContainer'
 import RecipePage from './recipes/RecipePage'
+import SignUp from './users/SignUp'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,6 +15,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <IndexRoute component={RecipesContainer} />
         <Route path="/recipes/:recipeId" component={RecipePage} />
+        <Route path="/sign-up" component={SignUp} />
       </Route>
     </Router>
   </Provider>,
