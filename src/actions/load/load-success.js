f2d161fc693de0cbadf5d46eb26dd4b47f705df5
Haico27 <haico.wensink@gmail.com>
2017-05-18 13:24:39 +0700

    created load-error and load-success actions and reducer

---
+++ src/actions/load/load-success.js
@@ -0,0 +1,8 @@
+export const LOAD_SUCCESS = 'LOAD_SUCCESS'
+
+export default () => {
+  return {
+    type: LOAD_SUCCESS,
+    payload
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import recipes from './recipes'
+import loading from './loading'
+import loadError from './loadError'
 
 module.exports = {
-  recipes
+  recipes,
+  loading,
+  loadError
 }
