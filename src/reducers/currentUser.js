d10075a058b1c0eebaaf16f95d4b2e4f7db80b8d
Haico27 <haico.wensink@gmail.com>
2017-05-19 16:44:30 +0700

    set initialState to null in currentUser reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,15 +2,16 @@ import { USER_SIGNED_IN } from '../actions/user/sign-in'
 
 const CURRENT_USER_KEY = 'recipes.currentUser'
 
-const initialState = JSON.parse(localStorage.getItem(CURRENT_USER_KEY))
+//const initialState = JSON.parse(localStorage.getItem(CURRENT_USER_KEY))
 
-export default ( state = initialState, { type, payload } ) => {
-
+export default ( state = null, { type, payload } ) => {
+  console.log("this is the state", state)
   switch(type) {
     case USER_SIGNED_IN:
       const currentUser = Object.assign({}, payload)
       localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(currentUser))
-      console.log(currentUser)
+      console.log("this is the localStorage", localStorage)
+      console.log("this is the currentUser", currentUser)
       return currentUser
 
     default:
